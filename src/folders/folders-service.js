9cc93efb4a05348505f61100a4f770c3225adb25
Paul Nissen <nissenbrau@gmail.com>
2020-04-17 15:28:31 -0600

    finished writing FoldersService.js and NotesService.js, added knex instance app.set to server.js

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,10 +1,41 @@
 const FoldersService = {
-  getAllFolders() {
-    return 'all the folders'
+  getAllFolders(knex) {
+    return knex
+      .select('*')
+      .from('folders')
+  },
+
+  insertFolder(knex, newFolder) {
+    return knex
+      .insert(newFolder)
+      .into('folders')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+
+  getById(knex, id) {
+    return knex
+    .from('folders')
+    .select('*')
+    .where({ id })
+    .first()
+  },
+
+  deleteFolder(knex, id) {
+    return knex
+      .from('folders')
+      .where({ id })
+      .delete()
+  },
+
+  updateFolder(knex, id, newFolderName) {
+    return knex
+      .from('folders')
+      .where({ id })
+      .update(newFolderName)
   }
-
-  
-
 }
 
 module.exports = FoldersService
--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -1,10 +1,41 @@
 const NotesService = {
-  getAllNotes() {
-    return 'all the notes'
+  getAllNotes(knex) {
+    return knex
+      .select('*')
+      .from('notes')
+  },
+
+  insertNote(knex, newNote) {
+    return knex
+      .insert(newNote)
+      .into('notes')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+
+  getById(knex, id) {
+    return knex
+    .from('notes')
+    .select('*')
+    .where({ id })
+    .first()
+  },
+
+  deleteNote(knex, id) {
+    return knex
+      .from('notes')
+      .where({ id })
+      .delete()
+  },
+
+  updateNote(knex, id, newNoteData) {
+    return knex
+      .from('notes')
+      .where({ id })
+      .update(newNoteData)
   }
-
-  
-
 }
 
 module.exports = NotesService
