9924e5381ab5614441b4c30c9e164f74a385fbba
Paul Nissen <nissenbrau@gmail.com>
2020-04-22 10:40:24 -0600

    finished code for folders-router.js, works in postman

---
+++ src/folders/folders-router.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const FoldersService = require('./folders-service');
+const path = require('path')
+
+const foldersRouter = express.Router();
+const jsonParser = express.json();
+
+foldersRouter
+  .route('/')
+  .get((req, res, next) => {
+    FoldersService.getAllFolders(req.app.get('db'))
+      .then(folders => {
+        res.json(folders)
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name } = req.body
+    const newFolder = { name }
+
+    if (!name || name.trim() == "") {
+      return res.status(400).json({
+        error: { message: 'Missing folder name in request body' }
+      })
+    }
+
+    FoldersService.insertFolder(req.app.get('db'), newFolder)
+    .then(folder => {
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+        .json(folder)
+    })
+    .catch(next)
+  })
+
+foldersRouter
+  .route('/:folder_id')
+  .all((req, res, next) => {
+    FoldersService.getById(
+      req.app.get('db'),
+      req.params.folder_id
+    )
+      .then(folder => { 
+        if (!folder) {
+          return res.status(404).json({
+            error: { message: `Folder doesn't exist` }
+          })
+        }
+        res.folder = folder
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+      res.json(res.folder)
+  })
+  .delete((req, res, next) => {
+    FoldersService.deleteFolder(
+      req.app.get('db'),
+      req.params.folder_id
+    )
+    .then(() => { 
+      res.status(204).end()
+    })
+    .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { name } = req.body
+    const newFolderName = { name }
+
+    if (!name || name.trim() == "") {
+      return res.status(400).json({
+        error: { message: `Request body must contain updated folder name` }
+      })
+    }
+
+    FoldersService.updateFolder(
+      req.app.get('db'),
+      req.params.folder_id,
+      newFolderName
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = foldersRouter
